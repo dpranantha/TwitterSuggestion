050e669ea0057d880837b0242b467b3f86d3e7d9
danu.pranantha@gmail.com <danu.pranantha@gmail.com>
2015-03-29 21:20:13 +0200

    Add twitter app consumer and filtering

--- twitterHarvesting.R
+++ twitterHarvesting.R
@@ -1,8 +1,17 @@
-# TODO: Add comment
-# 
 # Author: dpranantha
 ###############################################################################
 
-x = 5
-y = 6
+library(ROAuth)
+library(streamR)
 
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+consumer <- lapply(read.csv('keys.csv'),as.character)
+
+my_oauth <- OAuthFactory$new(consumerKey=consumer$consumerKey,
+		consumerSecret=consumer$consumerSecret, requestURL=requestURL,
+		accessURL=accessURL, authURL=authURL)
+my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+#my_oauth$handshake()
+filterStream( file="firehose.json",track="louis vuitton", oauth=my_oauth , tweets = 10)
